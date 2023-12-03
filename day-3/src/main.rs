7c1d25c4dd60572456ea5f398b61ff7eda46cb95
BlueZeeKing <github@blueish.dev>
2023-12-03 13:55:24 -0500

    day-3 part-1

---
+++ day-3/src/main.rs
@@ -0,0 +1,113 @@
+use std::{fs, ops::RangeInclusive};
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let sum: u32 = lines
+        .iter()
+        .enumerate()
+        .map(|(row, line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(|(col, char)| Some((col, char.to_digit(10)?)))
+                .map(move |(col, char)| ((col, row), char))
+        })
+        .flatten()
+        .grouping_iter()
+        .filter(|((x, y), number)| {
+            SurroundingIter::new(x.clone(), *y)
+                .map(|val| if *number == 660 { dbg!(val) } else { val })
+                .filter_map(|(x, y)| lines.get(y).and_then(|val| val.chars().nth(x)))
+                .any(|char| char != '.' && !char.is_digit(10))
+        })
+        .map(|(_, number)| number)
+        .sum();
+
+    println!("{}", sum);
+}
+
+struct SurroundingIter {
+    x: RangeInclusive<usize>,
+    y: usize,
+    cx: usize,
+    cy: usize,
+}
+
+impl SurroundingIter {
+    pub fn new(x: RangeInclusive<usize>, y: usize) -> Self {
+        Self {
+            cx: x.start().checked_sub(1).unwrap_or(*x.start()),
+            cy: y.checked_sub(1).unwrap_or(y),
+            x,
+            y,
+        }
+    }
+}
+
+impl Iterator for SurroundingIter {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cy > self.y + 1 {
+            return None;
+        }
+
+        let res = (self.cx, self.cy);
+
+        self.cx += 1;
+
+        if self.cx > self.x.end() + 1 {
+            self.cx = self.x.start().checked_sub(1).unwrap_or(*self.x.start());
+            self.cy += 1;
+        }
+
+        while self.cy == self.y && self.x.contains(&self.cx) {
+            self.cx;
+            self.cx += 1;
+        }
+
+        Some(res)
+    }
+}
+
+struct GroupingIter<I: Iterator<Item = ((usize, usize), u32)>> {
+    iter: I,
+    prev: Option<((usize, usize), u32)>,
+}
+
+impl<I: Iterator<Item = ((usize, usize), u32)>> Iterator for GroupingIter<I> {
+    type Item = ((RangeInclusive<usize>, usize), u32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ((mut x, y), mut val) = self.prev.take().or_else(|| self.iter.next())?;
+        let first_x = x;
+
+        loop {
+            let Some(((new_x, new_y), new_val)) = self.iter.next() else {
+                break;
+            };
+
+            if new_x == x + 1 && new_y == y {
+                x = new_x;
+                val = val * 10 + new_val;
+            } else {
+                self.prev = Some(((new_x, new_y), new_val));
+                break;
+            }
+        }
+
+        Some(((first_x..=x, y), val))
+    }
+}
+
+trait IntoGroupingIter: Iterator<Item = ((usize, usize), u32)> + Sized {
+    fn grouping_iter(self) -> GroupingIter<Self> {
+        GroupingIter {
+            iter: self,
+            prev: None,
+        }
+    }
+}
+
+impl<I: Iterator<Item = ((usize, usize), u32)> + Sized> IntoGroupingIter for I {}
