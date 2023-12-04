490f866b49d494ec16279d27ba291a37ba6c46fa
BlueZeeKing <github@blueish.dev>
2023-12-04 13:11:45 -0500

    day-4

---
+++ day-4/src/main.rs
@@ -0,0 +1,51 @@
+use std::{
+    collections::{hash_map::RandomState, HashSet},
+    fs,
+};
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let lines = input.lines();
+    let num_lines = input.lines().count();
+    let mut number_of_cards = vec![1; num_lines];
+
+    let scorecards = lines
+        .enumerate()
+        .filter_map(|(index, line)| {
+            let mut parts = line.split(": ");
+            let _start = parts.next().unwrap();
+            let mut numbers = parts.next().unwrap().split('|');
+            let winning = numbers.next().unwrap();
+            let have = numbers.next().unwrap();
+
+            let winning: HashSet<u32, RandomState> = HashSet::from_iter(
+                winning
+                    .trim()
+                    .split(' ')
+                    .filter_map(|name| name.parse::<u32>().ok()),
+            );
+
+            let have: HashSet<u32, RandomState> = HashSet::from_iter(
+                have.trim()
+                    .split(' ')
+                    .filter_map(|name| name.parse::<u32>().ok()),
+            );
+
+            let count = winning.intersection(&have).into_iter().count();
+
+            if count != 0 {
+                Some((index, count))
+            } else {
+                None
+            }
+        })
+        .for_each(|(index, score)| {
+            let number_for_line = number_of_cards[index];
+            for index in index + 1..=index + score {
+                number_of_cards[index] += number_for_line;
+            }
+        });
+
+    println!("{}", number_of_cards.iter().sum::<u32>())
+}
+
